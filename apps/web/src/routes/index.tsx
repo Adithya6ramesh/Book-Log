5e01fed53cdab3b824d7f47562f79330d20213ea
kasraghoreyshi <kasra.ghoreyshi84@gmail.com>
2025-05-02 13:38:23 +0330

    Refactor notes into their own components

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@repo/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { AuthShowcase } from "../components/auth-showcase";
 import { CreateNote } from "../components/create-note";
+import { Notes } from "../components/notes";
 import { notesQueryOptions } from "../queries/notes.queries";
 
 export const Route = createFileRoute("/")({
@@ -27,27 +21,7 @@ function Index() {
         <AuthShowcase />
       </div>
       <CreateNote />
-      {notesQuery.data?.map((note) => (
-        <Card
-          key={note.id}
-          className="w-full max-w-lg shadow-md hover:shadow-lg transition-shadow bg-transparent text-white border-zinc-600"
-        >
-          <CardHeader>
-            <CardTitle className="text-xl font-bold">{note.title}</CardTitle>
-            <CardDescription className="mt-2 text-gray-300">
-              {note.content}
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="text-sm text-gray-300 flex justify-between items-center border-t border-zinc-600 pt-3">
-            <span>Created by: {note.creatorName}</span>
-            {note.isOwner && (
-              <span className="text-blue-500 text-xs font-medium">
-                You are the owner
-              </span>
-            )}
-          </CardFooter>
-        </Card>
-      ))}
+      {notesQuery.data && <Notes notes={notesQuery.data} />}
     </div>
   );
 }
